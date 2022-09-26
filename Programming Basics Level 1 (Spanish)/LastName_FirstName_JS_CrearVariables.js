fd5dbeae474f298fdcd8206c93cf86e169af8a2f
Sebastian Prieto <35538199+SebaGPDev@users.noreply.github.com>
2022-09-26 20:58:18 -0300

    Code challenge: create variables 26/09/2022

---
+++ Programming Basics Level 1 (Spanish)/LastName_FirstName_JS_CrearVariables.js
@@ -0,0 +1,10 @@
+/*
+ * Analizando el caso del parque se podria utilizar dos variables
+ * una para la altura y otra para la edad.
+ * En este caso la altura podria estar expresada en centimetros para poder
+ * realizar una medicion mas precisa 
+ * 1 metro = 100 centimetros
+ */
+
+let altura = 100;
+let edad = 10;
