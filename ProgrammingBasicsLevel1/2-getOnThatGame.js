dd77ebc0366e07a5caa04f6523121a16d743b9d0
Sebastian Prieto <35538199+SebaGPDev@users.noreply.github.com>
2022-09-27 07:23:08 -0300

    Get on that game: Completed 27/09/2022

---
+++ ProgrammingBasicsLevel1/2-getOnThatGame.js
@@ -0,0 +1,7 @@
+alert("Bienvenido al parque!");
+let altura = parseInt(prompt("Ingrese su altura:"));
+let edad = parseInt(prompt("Ingrese su edad:"));
+
+altura > 52 || edad > 10
+  ? console.log("¡Súbete, chico!")
+  : console.log("Lo siento, chico. Tal vez el próximo año");
