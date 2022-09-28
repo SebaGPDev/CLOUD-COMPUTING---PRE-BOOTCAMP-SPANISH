bbc34ee58ee5a0679ad27916d39bd86a75825df8
Sebastian Prieto <35538199+SebaGPDev@users.noreply.github.com>
2022-09-28 14:12:37 -0300

    parameters complete

---
+++ ProgrammingBasicsLevel1/Prieto_Sebastian_parameters.js
@@ -0,0 +1,9 @@
+const saludar = () => {
+  let nombre = prompt("Ingrese su nombre");
+  let tiempo = new Date();
+  let ahora = tiempo.toLocaleTimeString();
+  nombre === "Count Dooku"
+    ? alert("¡Voy por ti, Dooku!")
+    : alert(`¡Buen día, ${nombre} ${ahora}`);
+};
+saludar();
