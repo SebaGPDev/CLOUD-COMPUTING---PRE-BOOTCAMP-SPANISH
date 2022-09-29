73cb2774ce98a8d6a0027978608dd5d09bd2c202
Sebastian Prieto <35538199+SebaGPDev@users.noreply.github.com>
2022-09-28 22:33:51 -0300

    update 28/09/2022

--- ProgrammingBasicsLevel1/Prieto_Sebastian_Repeat.js
+++ ProgrammingBasicsLevel1/Prieto_Sebastian_Repeat.js
@@ -26,3 +26,15 @@ for (let i = 0; i < arr3.length; i++) {
     console.log(arr3[i]);
   }
 }
+
+/* BONUS NINJA: modifica tu solución para el #3 para que cualquier número
+en el arreglo que NO sea mayor que 5 se reemplace 
+con una cadena de "No hay dados". Esta cadena NO debe imprimirse. */
+
+for (let i = 0; i < arr3.length; i++) {
+  if (arr3[i] > 5) {
+    console.log(arr3[i]);
+  } else {
+    arr3[i] = "No hay dados";
+  }
+}
