e104f64fa4ec1582c0558b66eee03fd82d0023e7
Sebastian Prieto <35538199+SebaGPDev@users.noreply.github.com>
2022-09-30 10:01:17 -0300

    changes 30/09/2022

--- ProgrammingBasicsLevel1/Prieto_Sebastian_LoopDLoop.js
+++ ProgrammingBasicsLevel1/Prieto_Sebastian_LoopDLoop.js
@@ -1,14 +1,14 @@
-/**
- * Sabemos que vamos a necesitar un bucle 
+/*
+ * Sabemos que vamos a necesitar un bucle
  * ya que necesitamos que se repita una condicion hasta cierto valor
-*/
+ */
 
 /**
  * En este caso tomo el punto de partida 1
  */
 
 /**
- * Se detendra 
+ * Se detendra
  */
 
 let caramelo = 0;
@@ -16,4 +16,20 @@ for (let i = 1; i < 10; i++) {
   i % 3 === 0 ? caramelo++ : false;
 }
 
-console.log(`la maquina dio ${caramelo} caramelos`)
+console.log(`la maquina dio ${caramelo} caramelos`);
+
+// Característica Stretch 1
+/*
+ * Crea un nuevo bucle en el que el corredor
+ * solo reciba un caramelo cada 3 kilómetros Y
+ * si se desplaza a más de 9 kilómetros por hora.
+ */
+
+var caramelo2 = 0;
+var velocidad = 10;
+for (var i = 1; i < 10; i++) {
+  i % 3 === 0 && velocidad > 9 
+  ? caramelo2++ 
+  : false;
+}
+console.log(`la maquina dio ${caramelo2} caramelos`);
