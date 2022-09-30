8d815583b4db9032e1ad503630385064844d0165
Sebastian Prieto <35538199+SebaGPDev@users.noreply.github.com>
2022-09-30 10:45:13 -0300

    update 30/09/2022

--- ProgrammingBasicsLevel1/Prieto_Sebastian_LoopDLoop.js
+++ ProgrammingBasicsLevel1/Prieto_Sebastian_LoopDLoop.js
@@ -1,14 +1,43 @@
 /*
+ * ¿Cómo sabemos que necesitamos un bucle aquí?
+ * ¿Cuál es el punto de partida del bucle?
+ * ¿Cuándo debe detenerse el bucle?
+ * ¿Cómo sabrá parar?
+ * ¿Cuál es el incremento para cada iteración del bucle?
+ * ¿Qué variables necesitamos?
+ * */
+
+/*
+ * ¿Cómo sabemos que necesitamos un bucle aquí?
  * Sabemos que vamos a necesitar un bucle
  * ya que necesitamos que se repita una condicion hasta cierto valor
  */
 
-/**
+/*
+ * ¿Cuál es el punto de partida del bucle?
  * En este caso tomo el punto de partida 1
  */
 
-/**
- * Se detendra
+/*
+ * ¿Cuándo debe detenerse el bucle?
+ * Se detendra cuando la condicion se cumpla, en este caso cuando sea menor a 10
+ */
+
+/*
+ * ¿Cómo sabrá parar?
+ * sabra para gracias a la condicion y que hara despues 
+ */
+
+/*
+ * ¿Cuál es el incremento para cada iteración del bucle?
+ * Por cada iteración aumenta con paso 1
+*/
+
+/*
+ * ¿Qué variables necesitamos?
+ * necesitamos una varible que nos servira como contador, otra para la velocidad
+ * para usarla con condicional
+ * y otra que nos servira para usarlo como indice en el bucle
  */
 
 let caramelo = 0;
@@ -28,8 +57,6 @@ console.log(`la maquina dio ${caramelo} caramelos`);
 var caramelo2 = 0;
 var velocidad = 10;
 for (var i = 1; i < 10; i++) {
-  i % 3 === 0 && velocidad > 9 
-  ? caramelo2++ 
-  : false;
+  i % 3 === 0 && velocidad > 9 ? caramelo2++ : false;
 }
 console.log(`la maquina dio ${caramelo2} caramelos`);
